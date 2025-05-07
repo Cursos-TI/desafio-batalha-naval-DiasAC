15132179e00bbb9312861fc43e58594586072407
Andressa Cinelli <acdias.96@outlook.com>
2025-05-07 14:33:16 +0000

    Nível Aventureiro concluido

--- Batalha_Naval_Novato.c
+++ Batalha_Naval_Novato.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 
 int main() {
-    char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};  // indicando as variaveis, usando vetor e matriz.
-    int tabuleiro[10][10] = {0}; 
-    int navio_vertical[3] = {7,8,9};
-    int navio_horizontal[3] = {5, 6, 7}; 
+    char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};  // Array(vetor) para indicar a coluna com letras.
+    int tabuleiro[10][10] = {0}; // matriz do tabuleiro no tamanho de 10x10.
+    int navio_vertical[3] = {7,8,9}; // variavel indicando o uso dos indices na vertical.
+    int navio_horizontal[3] = {5, 6, 7}; // variavel indicando o uso dos indices na horizontal.
 
     printf("*** Batalha Naval ***\n\n");
 
 
-    for (int i = 0; i < 3; i++) {                  // impressao dos navios no formato horizontal.
+    for (int i = 0; i < 3; i++){    // uso do loop for para imprimir o navio no formato diagonal primario.
+        tabuleiro[i][i] = 3;
+   
+    }
+   
+    for (int i = 0; i < 3; i++){   // uso do loop for para imprimir o navio no formato diagonal secundario.
+        tabuleiro[i][9 - i] = 3;
+    }
+
+    for (int i = 0; i < 3; i++) {                  // uso do loop for para imprimir o navio no formato horizontal
         tabuleiro[4] [navio_horizontal[i]] = 3;
         
     }
 
-    for (int i = 0; i < 3; i++){                 // impressao dos navios no formato vertical.
+    for (int i = 0; i < 3; i++){                 // uso do loop for para imprimir o navio no formato vertical
         tabuleiro[navio_vertical[i]][9] = 3;
     }
 
     printf("   ");
-    for (int i = 0; i < 10; i++) {       // exibiçao da coluna.
+    for (int i = 0; i < 10; i++) {       // uso do loop for para exibir a coluna.
         printf(" %c", coluna[i]);
     }
     printf("\n");
 
     
-    for (int i = 0; i < 10; i++) {             // exibiçao do tabuleiro.
+    for (int i = 0; i < 10; i++) {             // uso do loop aninhado para exibiçao do tabuleiro.
         printf("%2d ", i + 1);
         for (int j = 0; j < 10; j++) {
             printf(" %d", tabuleiro[i][j]);
