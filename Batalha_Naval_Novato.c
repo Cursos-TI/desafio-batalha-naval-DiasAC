3e1fb046e3b8888acc183e3a1c02f5b3179fd1dc
Andressa Cinelli <acdias.96@outlook.com>
2025-05-08 19:30:22 +0000

    Atualização

--- Batalha_Naval_Novato.c
+++ Batalha_Naval_Novato.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 
-int main() {
+int main(){
+
     char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};  // Array(vetor) para indicar a coluna com letras.
     int tabuleiro[10][10] = {0}; // matriz do tabuleiro no tamanho de 10x10.
     int navio_vertical[3] = {7,8,9}; // variavel indicando o uso dos indices na vertical.
     int navio_horizontal[3] = {5, 6, 7}; // variavel indicando o uso dos indices na horizontal.
+    int habilidade_cone [5][5];
+    int habilidade_cruz [5][5];
+    int habilidade_octaedro [5][5];
+
 
     printf("*** Batalha Naval ***\n\n");
+
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++){
+
+            if (i = 2 && j ==2){
+            habilidade_cruz[i][j] = 1;
+            } else {
+            habilidade_cruz[i][j] = 0;
+            }
+        }    
+            
+    }
 
 
     for (int i = 0; i < 3; i++){    // uso do loop for para imprimir o navio no formato diagonal primario.
